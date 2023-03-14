70991c5af054171b533a364a011160e1b644b868
Jonas Nilsson <jonas.nilsson@laroverken.se>
2023-03-14 11:58:40 +0100

    calendar

---
+++ BlazorTodoApp/Data/Test.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorTodoApp.Data
+{
+    public class Food
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Info { get; set; }
+        public List<FoodPrice> Prices { get; set; }
+    }
+
+    public class FoodPrice
+    {
+        public int Id { get; set; }
+        public int FoodId { get; set; }
+        public int Price { get; set; }
+        public string Info { get; set; }
+    }
+}
--- TodoAPI_Example/Tasks.cs
+++ TodoAPI_Example/Tasks.cs
@@ -22,12 +22,21 @@ namespace TodoAPI_Example
 
         private async void btnLoadTask_Click(object sender, EventArgs e)
         {
+
+            //vi skapar en klass för att göra ett anrop på nätet
             HttpClient httpClient = new HttpClient();
+
+            //URL.en där vår data hämtas ifrån
             string path = "https://localhost:7212/api/todoapi/listtasks";
+            
+            //Vi gör ett anrop och laddar ner data i response
             HttpResponseMessage response = await httpClient.GetAsync(path);
             if (response.IsSuccessStatusCode)
             {
+                //Om det gick bra - konvertera Jsoninformationen till en lista med TaskItem
                 TaskList = await response.Content.ReadFromJsonAsync<IEnumerable<TaskItem>>();
+
+                //Sätt TaskList som en datakälla till vår GridView
                 gridTasks.DataSource = TaskList;
 
             }
